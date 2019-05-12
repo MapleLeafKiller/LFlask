29aed3eac581ca884394ba4226488c5bf42e72ca
MapleLeafKiller <hhuwoyaoxuexi@163.com>
2019-05-13 03:08:22 +0800

    c

--- LFlask/setup.py
+++ LFlask/setup.py
@@ -1,14 +1,12 @@
-from flask import Flask, url_for
-from flask import render_template
-from flask import request
-from flask import abort, redirect, url_for
+from setuptools import find_packages, setup
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return redirect(url_for('login'))
-
-@app.route('/login')
-def login():
-    abort(401)
+setup(
+    name='flaskr',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
